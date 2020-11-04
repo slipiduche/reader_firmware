20a16af92c55a6d565c161e8b8fbce2ef8ee6f9d
Alejandro Camacaro <alejandrocamacaro91@gmail.com>
2020-11-04 17:45:48 -0400

    command

--- fun_loadmqttcommand.ino
+++ fun_loadmqttcommand.ino
@@ -3,12 +3,13 @@ void loadMqttCommand(String datar)
     if (modo_nowc == 0)
     {
 
-        DEBUG_PRINTLN("Comando recibido--Ignorado por control");
+        DEBUG_PRINTLN("Ignored...");
     }
     else if (datar.startsWith("{\"REQUEST\":\"INFO\"}"))
     {  
         serverPoll=1;
         Serial.print("llego poll");
     }
+    
     
 }
