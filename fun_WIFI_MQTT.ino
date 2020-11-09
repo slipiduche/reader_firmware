594eff54010464bc5e93fc53b08a1b7c39209e94
Alejandro Camacaro <alejandrocamacaro91@gmail.com>
2020-11-09 12:23:12 -0400

    ok

--- fun_WIFI_MQTT.ino
+++ fun_WIFI_MQTT.ino
@@ -245,8 +245,12 @@ void wifi_mqtt_loop()
     if (serverPoll)
     {
 
+<<<<<<< HEAD
       if (wifi_mqtt_publish(("READER/INFO"), "{\"NAME\": \"" + String(devName) + "\",\"CHIP_ID\":\"" + chipid + "\"}"))
 
+=======
+      if (wifi_mqtt_publish(("READER/INFO"), "{\"NAME\": \"READER\",\"CHIP_ID\":\"" + chipid + "\"}"))
+>>>>>>> parent of 15a661a... register
       {
         Serial.print("SE ENVIO POLL");
         serverPoll = 0;
