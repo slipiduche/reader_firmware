c260183fdce5a66b25e3479b89bf44ca661e335f
slipiduche <48814217+slipiduche@users.noreply.github.com>
2021-03-17 16:10:37 -0400

    350ms

--- reader_firmware.ino
+++ reader_firmware.ino
@@ -36,7 +36,7 @@ void loop() ///nfc LOOP
     ESP.restart();
   }
 
-  if ((abs(millis() - nfcDelay) >= 150) && (bussyMqtt == 0) && (apMode == 2))
+  if ((abs(millis() - nfcDelay) >= 350) && (bussyMqtt == 0) && (apMode == 2))
   {
     // claimSPI("NFC"); // Claim SPI bus
     tagId = nfc_Loop();
